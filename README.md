c6d32b24bb0d178ca3a78a6cc56244e2f6402738
Harry <rattansingh1699@gmail.com>
2024-04-16 16:36:16 +0530

    Read me updated

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 A GitHub repo demonstrating seamless integration of fingerprint scanning into Android apps.
+
+### Installation
+```bash
+dependencies {
+      implementation 'com.github.legend295.FingerprintScannerSDK:scanner:main-SNAPSHOT'
+}
+```
+
+```settings.gradle
+ maven {
+       url 'https://jitpack.io'
+       credentials { username authToken }
+}
+
+```
+
+Add the above line to your project's `build.gradle` file and `settings.gradle` file to include FingerprintScannerLib in your Android project.
+
+**authToken will be provided by our support.** 
--- app/src/main/java/com/waxdscannerpoc/android/MainActivity.kt
+++ app/src/main/java/com/waxdscannerpoc/android/MainActivity.kt
@@ -1,11 +1,46 @@
 package com.waxdscannerpoc.android
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.widget.AppCompatTextView
+import com.scanner.activity.ScannerActivity
+import com.scanner.utils.constants.ScannerConstants
+import java.io.File
+import java.lang.StringBuilder
 
 class MainActivity : AppCompatActivity() {
+
+    private var tvStatus: AppCompatTextView? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val tvStartScan: Button = findViewById(R.id.btnStartScan)
+        tvStatus = findViewById(R.id.tvStatus)
+        tvStartScan.setOnClickListener {
+            scanningLauncher.launch(Intent(this, ScannerActivity()::class.java))
+        }
+    }
+
+    private val scanningLauncher =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+            if (it.resultCode == RESULT_OK) {
+                val list: ArrayList<File>? = it.data?.serializable(ScannerConstants.DATA)
+                Log.d(MainActivity::class.simpleName, list?.size.toString())
+                handleResponse(list)
+            }
+        }
+
+    private fun handleResponse(list: ArrayList<File>?) {
+        var message = ""
+        list?.forEach {
+            message += "\n${it.path}"
+        }
+        if (message.isNotEmpty()) {
+            tvStatus?.text = StringBuilder().append("File saved to paths :- ").append(message)
+        }
     }
 }
